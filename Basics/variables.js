34b2a5a29a68815eb323b158e62187872c623370
Atanu Hazra <imatanuhazra@gmail.com>
2024-02-29 17:01:28 +0530

    first commit

---
+++ Basics/variables.js
@@ -0,0 +1,20 @@
+const accountId = 87383
+let accountEmail = "xyz@google.com"
+var accountPassword = "12345"
+// accountCity = "Delhi" // though we can initialize a variable without using let, var, or const by we should not do that
+let accountName;
+
+// modefying the values
+accountEmail = "abc@gmail.com"
+console.log(accountEmail)
+console.log(accountName) // undefined
+
+console.table([accountId, accountEmail, accountPassword])
+
+// Data types
+
+console.log(typeof accountId)
+console.log(typeof "Gour-Nitai")
+console.log(typeof undefined) //undefined
+console.log(typeof "Gour-Nitai") //string
+console.log(typeof null) //object
