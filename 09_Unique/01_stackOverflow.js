09bc66f44d748bd93f9e02a3754e76d7f80bf0d3
Atanu Hazra <imatanuhazra@gmail.com>
2024-04-18 22:40:51 +0530

    Updating...

---
+++ 09_Unique/01_stackOverflow.js
@@ -0,0 +1,10 @@
+// A stack overflow in JavaSCript Occurs when the call stack exceeds its maximum size due to too many function calls being added without being completed.
+
+function infinitLoop() {
+    infinitLoop();
+    console.log("What is happening man!!!")
+}
+
+infinitLoop(); // Error: Maximum call stack size exceeded
+
+// How to Prevent --> To prevent stack overflow errors, make sure that the recursive function have proper termination conditions to stop the recursion.
