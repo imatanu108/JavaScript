01d78e8a15f64e1da709e53bf6a623480768a7b9
Atanu Hazra <imatanuhazra@gmail.com>
2024-04-23 20:10:31 +0530

    Updating...

--- 03_Main/03_Objects_Prototype.js
+++ 03_Main/03_Objects_Prototype.js
@@ -43,28 +43,37 @@ Car.prototype.refuel = function() {
     console.log(`${this.brand} ${this.model} is refuled.`)
 }
 // this refuel() method is now added to the prototype of the Car contructor, so we can access this method by any object(instances) created from this contructor
+
 car1.refuel(); // Toyota Fortunar is refuled.
 car2.refuel(); // Tata Harier is refuled.
 
 // Prototype Object
+
 class AppUser {
     constructor(name, age) {
         this.name = name;
         this.age = age;
     }
 }
+
+// adding method to the prototype of the Constructor
+
 AppUser.prototype.logIn = function() {
     console.log(`${this.name} is logged in successfully.`)
-} // adding method to the prototype of the Constructor
+}
+
 let user1 = new AppUser("Hitesh", 27)
 let user2 = new AppUser("Anish", 22)
+
 // accessing the prototypr method
 user1.logIn(); // Hitesh is logged in successfully.
 
 // Getting the prototype methods of the Constructor
 console.log(AppUser.prototype); // { logIn: [Function (anonymous)] }
+
 // Getting the prototype methods of the object instances created from the Constructor
 console.log(user1.prototype) // undefined - can't be accessed directly
+
 let user1Prototype = Object.getPrototypeOf(user1)
 console.log(user1Prototype); // { logIn: [Function (anonymous)] }
 
@@ -76,6 +85,7 @@ console.log(user1.hasOwnProperty("name")) // true
 user1Prototype.logOut = function(){
     console.log(`${this.name} is logged out successfully.`)
 }
+
 console.log(user1Prototype) // { logIn: [Function (anonymous)], logOut: [Function (anonymous)] } - new mwthod added
 console.log(AppUser.prototype) // { logIn: [Function (anonymous)], logOut: [Function (anonymous)] } - here this method is also added in the prototype of AppUser Constructor - This is called Prototype Chaining
 user1.logOut(); // Hitesh is logged out successfully.
