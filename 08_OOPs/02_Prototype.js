4c99b02e183383ac5f05c5e43ac43dd4f0ffc639
Atanu Hazra <imatanuhazra@gmail.com>
2025-01-02 13:46:37 +0530

    fix .

--- 08_OOPs/02_Prototype.js
+++ 08_OOPs/02_Prototype.js
@@ -1,60 +1,60 @@
-// // More on 'new' and 'this' keyword, prototypes
+// More on 'new' and 'this' keyword, prototypes
 
-// // In JavaScript everthing(String, Array, Functions) is reffered to an Object, so actually we can treat them as objects and use the methods and properties of objects if possible
+// In JavaScript everthing(String, Array, Functions) is reffered to an Object, so actually we can treat them as objects and use the methods and properties of objects if possible
 
-// // Try to treating an Function as an Object
+// Try to treating an Function as an Object
 
-// function multiplyBy5 (num){
-//     return num*5
-// }
+function multiplyBy5 (num){
+    return num*5
+}
 
-// // lets adding a property into this function like we do in an Object so we can check the behavior of the function as an Object
+// lets adding a property into this function like we do in an Object so we can check the behavior of the function as an Object
 
-// multiplyBy5.power = 2
+multiplyBy5.power = 2
 
-// console.log(multiplyBy5(7)); // 35
-// console.log(multiplyBy5.power); // 2 - we are getting output 2, that does mean its actually working like an Object
+console.log(multiplyBy5(7)); // 35
+console.log(multiplyBy5.power); // 2 - we are getting output 2, that does mean its actually working like an Object
 
-// // actually the main reason behind this is the prototype of the Object is directly link to Strings, Array and Function like a parents prototype so we can access the prototype methods and properties of the Object via strings, arrays and functions.
+// actually the main reason behind this is the prototype of the Object is directly link to Strings, Array and Function like a parents prototype so we can access the prototype methods and properties of the Object via strings, arrays and functions.
 
-// // (Strings, Arrays, Functions) --> Object --> null (Prototypal inheritance in JavaScript) : By this chain we can understand that if we try to access a method in Strings, Arrays or Functions which is directly not available in Strings, Arrays or Functions methods, it will look for this method into their Parents Prototype (i.e. Object)
+// (Strings, Arrays, Functions) --> Object --> null (Prototypal inheritance in JavaScript) : By this chain we can understand that if we try to access a method in Strings, Arrays or Functions which is directly not available in Strings, Arrays or Functions methods, it will look for this method into their Parents Prototype (i.e. Object)
 
-// console.log(multiplyBy5.prototype); // {} - Here by default some contexts(properties and methods) are set to this prototype [Check in Browser for get the methods], some methods are set from its parent's prototype
+console.log(multiplyBy5.prototype); // {} - Here by default some contexts(properties and methods) are set to this prototype [Check in Browser for get the methods], some methods are set from its parent's prototype
 
-// function Item (name, price) {
-//     this.name = name;
-//     this.price = price;
-// }
+function Item (name, price) {
+    this.name = name;
+    this.price = price;
+}
 
-// // accessing its prototype and adding some methods
-// Item.prototype.getPrice = function (){
-//     console.log(`The price of ${this.name} is ${this.price} Rupees.`)
-// }
-// Item.prototype.increasePrice = function (increament){
-//     this.price = this.price + increament;
-// }
+// accessing its prototype and adding some methods
+Item.prototype.getPrice = function (){
+    console.log(`The price of ${this.name} is ${this.price} Rupees.`)
+}
+Item.prototype.increasePrice = function (increament){
+    this.price = this.price + increament;
+}
 
-// // Now creating some instances and accessing the prototype methods
-// const iPhone15 = new Item('iPhone 15', 99999)
+// Now creating some instances and accessing the prototype methods
+const iPhone15 = new Item('iPhone 15', 99999)
 
-// iPhone15.getPrice(); // The price of iPhone 15 is 99999 Rupees.
-// iPhone15.increasePrice(1000);
-// iPhone15.getPrice(); // The price of iPhone 15 is 100999 Rupees.
+iPhone15.getPrice(); // The price of iPhone 15 is 99999 Rupees.
+iPhone15.increasePrice(1000);
+iPhone15.getPrice(); // The price of iPhone 15 is 100999 Rupees.
 
 
-// // Advanced Prototypes
-// // lets add some methods to the prototype of the Global Object, and try to access them in Strings, Arrays and Functions
+// Advanced Prototypes
+// lets add some methods to the prototype of the Global Object, and try to access them in Strings, Arrays and Functions
 
-// Object.prototype.sayHello = function (){
-//     console.log("Hello!");
-// }
-// const myString = ""
-// const myArray = []
-// function myFunction (){}
+Object.prototype.sayHello = function (){
+    console.log("Hello!");
+}
+const myString = ""
+const myArray = []
+function myFunction (){}
 
-// myString.sayHello(); // Hello!
-// myArray.sayHello(); // Hello!
-// myFunction.sayHello(); // Hello!
+myString.sayHello(); // Hello!
+myArray.sayHello(); // Hello!
+myFunction.sayHello(); // Hello!
 
 const str1 = "Hello       "
 const str2 = "How are you?       "
